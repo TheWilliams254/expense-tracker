4a7d2b3a36a05d3adfbeb92855a3ab620064a904
william <williamwambugu663@gmail.com>
2025-04-14 18:15:29 +0300

    changing <p>

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,9 @@ const App = () => {
   return (
     <div className="p-6 md:p-12 bg-gray-50 min-h-screen font-sans">
       <h1 className="text-4xl font-bold mb-2">Expense Tracker</h1>
-      <p className="mb-6 text-gray-500">Start taking control of your finances and life...</p>
+      <p className="mb-6 text-gray-500">Start taking control of your finances and life. Record,
+      categorize and analyze your spending.
+      </p>
 
       <div className="grid md:grid-cols-2 gap-8">
         <ExpenseForm onAddExpense={handleAddExpense} />
